9b3b2cc7050c2aaee3c40e0996e157875924ea90
AGhost-7 <jonathan.boudreau.92@gmail.com>
2016-03-10 21:22:42 -0500

    Foobar

--- publish-doc.sh
+++ publish-doc.sh
@@ -2,6 +2,10 @@ read -p 'Commit message: ' msg
 if [ -f doc ]; then
 	rm -rf doc
 fi
+if [[ `git rev-parse --abbrev-ref HEAD` != "master" ]]; then
+	git checkout master || exit 1
+fi
+
 npm run doc
 git add -A
 git commit -m "$msg"
@@ -12,3 +16,6 @@ rmdir doc
 git add -A
 git commit -m "$msg"
 git push origin gh-pages
+git checkout master
+# And finally remove the docs...
+git reset --hard HEAD~1
